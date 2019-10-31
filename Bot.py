9098b05e68973cf9e44434b7c210e6d5b1a6745b
Bendik <knaben@OBOS.NO>
2019-10-31 08:10:22 +0100

    added load_driver

--- Bot.py
+++ Bot.py
@@ -1,26 +1,47 @@
 from selenium import webdriver
 
 
-def main():
+class Bot:
+    id = 0
+    def __init__(self):
+        self.id = id+1
+        id += 1
+
+
+def load_driver(self, url:str = None, headless: bool = False):
+    """loads driver and opens url, default to cms url if not provided
+    
+    Keyword Arguments:
+        url {str} -- url to be loaded (default: {None})
+        headless {bool} -- set to True if you want to run in headless mode (default: {False})
+    """
     options = webdriver.ChromeOptions()
-    #options.add_argument("headless")
+    if self.headless:
+        options.add_argument("headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--log-level=3")
     options.add_argument("--silent")
     driver = webdriver.Chrome(
         executable_path=r"C:\Users\knaben\AppData\Local\chromedriver.exe",
         options=options,
-        )  # options=options
+        )
+    if not url:
+        url: str = "https://cms.obos.no/EPiServer/Cms/#context=epi.cms.contentdata:///5&viewsetting=viewlanguage:///no" 
+    driver.get(url)
 
-    url: str = "https://cms.obos.no/EPiServer/Cms/#context=epi.cms.contentdata:///5&viewsetting=viewlanguage:///no" 
+    def open_additem():
+        """
+        Navigates the menu and opens the add item page
+        
+        """
 
-    meny=driver.find_element_by_class_name("epi-iconTree")
-    meny.click()
-    omdirigeringer = driver.find_element_by_id("uniqName_62_0_tablist_uniqName_2_12")
-    omdirigeringer.click()
-    obosfelt = driver.find_element_by_css_selector("#dijit_InlineEditBox_31")
-    obosfelt.click()
-    dropdown = driver.find_elements_by_css_selector("#uniqName_0_66 > div.dijitTreeRow.dijitTreeRowSelected > span > span.epi-extraIconsContainer > span.epi-extraIcon.dijitTreeIcon.epi-iconContextMenu")
-    dropdown[0].click()
-    additem = driver.find_element_by_css_selector("#uniqName_37_40_text")
-    additem.click()
+        meny=driver.find_element_by_class_name("epi-iconTree")
+        meny.click()
+        omdirigeringer = driver.find_element_by_id("uniqName_62_0_tablist_uniqName_2_12")
+        omdirigeringer.click()
+        obosfelt = driver.find_element_by_css_selector("#dijit_InlineEditBox_31")
+        obosfelt.click()
+        dropdown = driver.find_elements_by_css_selector("#uniqName_0_66 > div.dijitTreeRow.dijitTreeRowSelected > span > span.epi-extraIconsContainer > span.epi-extraIcon.dijitTreeIcon.epi-iconContextMenu")
+        dropdown[0].click()
+        additem = driver.find_element_by_css_selector("#uniqName_37_40_text")
+        additem.click()

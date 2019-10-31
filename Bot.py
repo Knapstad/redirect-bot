04b012f5ffc20b6496d8c326da77ea6284193935
Bendik <knaben@OBOS.NO>
2019-10-31 08:40:56 +0100

    added self to driver

--- Bot.py
+++ Bot.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 class Bot:
     id = 0
     def __init__(self):
-        self.id = id+1
-        id += 1
+        self.id = Bot.id+1
+        Bot.id += 1
 
 
 
@@ -17,32 +17,42 @@ class Bot:
             headless {bool} -- set to True if you want to run in headless mode (default: {False})
         """
         options = webdriver.ChromeOptions()
-        if self.headless:
+        if headless:
             options.add_argument("headless")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--log-level=3")
         options.add_argument("--silent")
-        driver = webdriver.Chrome(
+        self.driver = webdriver.Chrome(
             executable_path=r"C:\Users\knaben\AppData\Local\chromedriver.exe",
             options=options,
             )
         if not url:
             url: str = "https://cms.obos.no/EPiServer/Cms/#context=epi.cms.contentdata:///5&viewsetting=viewlanguage:///no" 
-        driver.get(url)
+        self.driver.get(url)
 
-    def open_additem():
+    def open_additem(self):
         """
         Navigates the menu and opens the add item page
         
         """
 
-        meny=driver.find_element_by_class_name("epi-iconTree")
+        meny=self.driver.find_element_by_class_name("epi-iconTree")
         meny.click()
-        omdirigeringer = driver.find_element_by_id("uniqName_62_0_tablist_uniqName_2_12")
+        omdirigeringer = self.driver.find_element_by_id("uniqName_62_0_tablist_uniqName_2_12")
         omdirigeringer.click()
-        obosfelt = driver.find_element_by_css_selector("#dijit_InlineEditBox_31")
+        obosfelt = self.driver.find_element_by_css_selector("#dijit_InlineEditBox_31")
         obosfelt.click()
-        dropdown = driver.find_elements_by_css_selector("#uniqName_0_66 > div.dijitTreeRow.dijitTreeRowSelected > span > span.epi-extraIconsContainer > span.epi-extraIcon.dijitTreeIcon.epi-iconContextMenu")
+        dropdown = self.driver.find_elements_by_css_selector("#uniqName_0_66 > div.dijitTreeRow.dijitTreeRowSelected > span > span.epi-extraIconsContainer > span.epi-extraIcon.dijitTreeIcon.epi-iconContextMenu")
         dropdown[0].click()
-        additem = driver.find_element_by_css_selector("#uniqName_37_40_text")
+        additem = self.driver.find_element_by_css_selector("#uniqName_37_40_text")
         additem.click()
+
+    def add_redirect(self, old: str, new: str):
+        """adds redirects
+        
+        Arguments:
+            old {str} -- old url
+            new {str} -- new url
+        """
+        new_url = self.driver.find_element_by_css_selector("#dijit_form_ValidationTextBox_3")
+        

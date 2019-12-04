591fd5738d91c4aaeee583054d5c8e9e695a7029
Bendik <knaben@OBOS.NO>
2019-12-04 14:08:12 +0100

    test script

--- redirtest.py
+++ redirtest.py
@@ -1,17 +1,24 @@
 import requests
 import csv
+import pandas as pd
 
 base = "https://www.obos.no"
 
 links = []
 responses = {"400":[], "200": [], "other": []} 
 
-with open("redir.csv") as f:
+with open("master.csv") as f:
         lines = csv.reader(f)
         for line in lines:
-            links.append(line)
+            if len(line)>0:
+                if "ï»¿" in line[0] or "gammel URL" in line[0]:
+                    pass
+                else:
+                    links.append(line[0].split("|"))
+            else:
+                pass
 
-for link in links[1:]:
+for link in links:
     link1= base+link[0]
     a = requests.get(link1)
     if a.status_code == 200:
